9213fd7480898639b926de4d5bee0d3f0edcef46
taeseob <tsshin1985@gmail.com>
2017-12-31 11:08:02 +0900

    Task, TaskTest 정리

--- src/main/java/net/taeseob/taskmanager/Task.java
+++ src/main/java/net/taeseob/taskmanager/Task.java
@@ -1,44 +1,17 @@
 package net.taeseob.taskmanager;
 
-import java.util.Date;
-import java.util.Set;
 import java.util.UUID;
 
 public class Task {
 	private UUID taskId;
-	private UUID taskGroupId;
-	private Set<UUID> subTaskIds;
 	private String title;
 	private String description;
-	private Date dueDate;
-	private Integer priority;
-	private ImportanceType importance;
-	private Set<UUID> priorGroupIds;
-	private Set<UUID> subsequentGroupIds;
-	// taskGroup, subTask, prior, subseq : 다른 객체가 관리하도록 할 수 있지 않을까? 책임?
-	// dueDate, priority, importance는 어떨까?
-	
+
 	public Task() {
 		this.taskId = UUID.randomUUID();
 	}
-	
 	public UUID getTaskId() {
 		return taskId;
-	}
-//	public void setTaskId(UUID taskId) {
-//		this.taskId = taskId;
-//	}
-	public UUID getTaskGroupId() {
-		return taskGroupId;
-	}
-	public void setTaskGroupId(UUID taskGroupId) {
-		this.taskGroupId = taskGroupId;
-	}
-	public Set<UUID> getSubTaskIds() {
-		return subTaskIds;
-	}
-	public void setSubTaskIds(Set<UUID> subTaskIds) {
-		this.subTaskIds = subTaskIds;
 	}
 	public String getTitle() {
 		return title;
@@ -52,34 +25,41 @@ public class Task {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public Date getDueDate() {
-		return dueDate;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((taskId == null) ? 0 : taskId.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
 	}
-	public void setDueDate(Date dueDate) {
-		this.dueDate = dueDate;
-	}
-	public Integer getPriority() {
-		return priority;
-	}
-	public void setPriority(Integer priority) {
-		this.priority = priority;
-	}
-	public ImportanceType getImportance() {
-		return importance;
-	}
-	public void setImportance(ImportanceType importance) {
-		this.importance = importance;
-	}
-	public Set<UUID> getPriorGroupIds() {
-		return priorGroupIds;
-	}
-	public void setPriorGroupIds(Set<UUID> priorGroupIds) {
-		this.priorGroupIds = priorGroupIds;
-	}
-	public Set<UUID> getSubsequentGroupIds() {
-		return subsequentGroupIds;
-	}
-	public void setSubsequentGroupIds(Set<UUID> subsequentGroupIds) {
-		this.subsequentGroupIds = subsequentGroupIds;
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (taskId == null) {
+			if (other.taskId != null)
+				return false;
+		} else if (!taskId.equals(other.taskId))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
 	}
 }
--- src/test/java/net/taeseob/taskmanager/TaskTest.java
+++ src/test/java/net/taeseob/taskmanager/TaskTest.java
@@ -10,6 +10,10 @@ public class TaskTest {
 	String title = "title";
 	String description = "This is my new Task";
 	
+	Task taskGroup = new Task();
+	Task subTask1 = new Task();
+	Task subTask2 = new Task();
+	
 	@Test
 	public void test_set_task_name() {
 		task.setTitle(title);
@@ -34,5 +38,28 @@ public class TaskTest {
 	public void test_diff_task_id() {
 		Task task2 = new Task();
 		assertFalse(task.getTaskId().equals(task2.getTaskId()));
+	}
+	
+	@Test(expected=DuplicatedTaskIdException.class)
+	public void test_register_duplicated_task() throws DuplicatedTaskIdException {
+		Task tempTask = new Task();
+		TaskManager.registerTask(tempTask);
+		TaskManager.registerTask(tempTask);
+	}
+	
+	@Test
+	public void test_add_sub_task_to_task_group() {
+		assertTrue(TaskManager.addSubTask(taskGroup.getTaskId(), subTask1.getTaskId(), subTask2.getTaskId()));
+	}
+	
+	@Test(expected=DuplicatedTaskIdException.class)
+	public void test_add_duplicated_sub_task_to_task_group() {
+		TaskManager.addSubTask(taskGroup.getTaskId(), subTask1.getTaskId(), subTask1.getTaskId());
+	}
+	
+	@Test
+	public void test_get_sub_task_from_task_group() {
+		assertTrue(TaskManager.getSubTask(taskGroup.getTaskId()).contains(subTask1.getTaskId()));
+		assertTrue(TaskManager.getSubTask(taskGroup.getTaskId()).contains(subTask2.getTaskId()));
 	}
 }
